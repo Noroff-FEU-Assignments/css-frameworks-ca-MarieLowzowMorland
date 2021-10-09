6e5648b4a73a66a2ce289225f02595ba78671cb9
Marie Lowzow Morland <marie@morland.no>
2021-10-09 10:54:37 +0200

    Nav-structure complete

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,16 @@
 import { NavLink } from 'react-router-dom';
 import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import FormControl from 'react-bootstrap/FormControl';
+import FormGroup from 'react-bootstrap/FormGroup';
+import FormLabel from 'react-bootstrap/FormLabel';
 import Container from "react-bootstrap/Container";
+import NavItem from 'react-bootstrap/NavItem';
 import Navbar from "react-bootstrap/Navbar";
 import NavbarBrand from "react-bootstrap/NavbarBrand";
 import NavbarToggle from 'react-bootstrap/NavbarToggle';
 import NavbarCollapse from 'react-bootstrap/NavbarCollapse';
+import Button from "react-bootstrap/Button";
 
 export const pages = {
     HOME: {
@@ -28,23 +34,32 @@ function Link(props) {
     const { name, path, exact} = props.page;
 
     return (
-        <NavLink exact={exact} to={path} activeClassName="active">{name}</NavLink>
+        <NavItem as="li">
+            <NavLink exact={exact} to={path} activeClassName="active" className="nav-link">{name}</NavLink>
+        </NavItem>
     );
 }
 
 function Header() {
     return (
         <header>
-            <Navbar expand="lg">
+            <Navbar expand="md">
                 <Container fluid>
-                    <NavbarBrand href="/">Logo</NavbarBrand>
+                    <NavbarBrand href="/">The YAY Company</NavbarBrand>
                     <NavbarToggle aria-controls="basic-navbar-nav"/>
                     <NavbarCollapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
+                        <Nav as="ul" className="me-auto">
                             <Link page={pages.HOME}/>
                             <Link page={pages.NEWS}/>
                             <Link page={pages.CONTACT}/>
                         </Nav>
+                        <Form className="d-flex">
+                            <FormGroup controlId="nav-search">
+                                <FormLabel visuallyHidden>Search</FormLabel>
+                                <FormControl type="text" placeholder="Search"/>
+                            </FormGroup>
+                            <Button type="submit">Go</Button>
+                        </Form>
                     </NavbarCollapse>
                 </Container>
             </Navbar>
